2222ecef04182554e6938b9a8c61fc150c23c31f
minmingsheng <shengminming1990@gmail.com>
2016-03-15 22:06:53 -0400

    s

--- www/config.php
+++ www/config.php
@@ -1,5 +1,5 @@
 <?php
-    $db = new PDO('mysql:host=http://192.168.2.29/; dbname=devgo; charset=utf8', "root", "root");
+    $db = new PDO('mysql:host=localhost; dbname=devgo; charset=utf8', "root", "root");
     $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     $db->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
 ?>
